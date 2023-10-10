41f340d9ec7ee3191819094fc0d48b1aa72ecccb
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-10-10 18:00:07 +0300

    add regreshing))

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,23 @@ import ContactsPage from 'pages/ContactsPage';
 import HomePage from 'pages/HomePage';
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
+
 import { refreshUser } from 'redux/auth/operations';
 import { RestrictedRoute } from './RestrictedRoute';
 import { PrivateRoute } from './PrivateRoute';
+import { useAuth } from 'hooks/useAuth';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
